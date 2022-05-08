d4da1aab1f2e6f746d281bfcc4e7037f18929098
konan080211 <konanforbis@gmail.com>
2022-05-08 12:34:29 +0900

    update: template

--- template/c.go
+++ template/c.go
@@ -4,17 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func input(sc *bufio.Scanner) string {
-	sc.Scan()
-	N := sc.Text()
-	return N
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+	solve()
 }
 
-func main() {
-	file, _ := os.Open("test.txt")
-	sc := bufio.NewScanner(file)
-	a := input(sc)
-	fmt.Println(a)
+// ファイル入力
+//var sc = bufio.NewScanner(os.Stdin)
+
+// 標準入力
+var file, _ = os.Open("test.txt")
+var sc = bufio.NewScanner(file)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	a, _ := strconv.Atoi(next())
+	return a
+}
+
+func nextInts(n int) []int {
+	ret := make([]int, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextInt()
+	}
+	return ret
+}
+
+func solve() {
+	N := nextInt()
+	fmt.Println(N)
 }
